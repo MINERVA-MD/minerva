405bd1a92baf69b685b892059f05493d58bf8381
Matteo Miceli <matteo@pop-os.localdomain>
2022-03-10 04:20:45 -0800

    editor merged with socketservice -socket service removed - fixing minor bugs

--- src/renderer/src/services/editor.service.ts
+++ src/renderer/src/services/editor.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 /* eslint-disable import/no-relative-packages */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable class-methods-use-this */
@@ -7,12 +8,14 @@ import {
 	collab,
 	getSyncedVersion,
 	type Update,
+	receiveUpdates,
 } from '@codemirror/collab';
 import { markdown } from '@codemirror/lang-markdown';
 import { Text } from '@codemirror/text';
 import { ViewPlugin } from '@codemirror/view';
-import type { Socket } from 'socket.io-client';
+import { io, Socket } from 'socket.io-client';
 import { marked } from 'marked';
+import { ChangeSet } from '@codemirror/state';
 
 export default class EditorService {
 	doc: Text;
@@ -21,32 +24,39 @@ export default class EditorService {
 
 	socket: Socket | null;
 
-	view: EditorView | null = null;
+	roomId = '';
+
+	view: EditorView;
 
 	vueComponent: any;
 
 	constructor(
 		vueComponent: any,
 		documentData: { doc: string[]; updates: Update[] },
-		socket: Socket | null = null,
 	) {
 		this.doc = Text.of(documentData.doc);
 		this.updates = documentData.updates;
-		this.socket = socket;
 		this.vueComponent = vueComponent;
 		const documenString = documentData.doc.join('\n');
 		this.vueComponent.parsedHTML = marked.parse(documenString);
+		this.view = new EditorView();
 	}
 
-	generateEditor() {
+	generateEditor(doc?: Text) {
+		let docText: Text = this.doc;
+		if (doc) {
+			docText = doc;
+		}
+		console.log(docText);
+
 		const state = EditorState.create({
-			doc: this.doc,
+			doc: docText,
 			extensions: [
 				basicSetup,
 				markdown(),
 				collab({ startVersion: this.updates.length }),
 				EditorView.lineWrapping,
-				this.editorClient(this.vueComponent, this.socket),
+				this.editorClient(this.vueComponent),
 			],
 		});
 
@@ -59,11 +69,10 @@ export default class EditorService {
 		return view;
 	}
 
-	setDocumentState(documentData: { doc: string[]; updates: Update[] }) {
-		
-	}
+	// setDocumentState(documentData: { doc: string[]; updates: Update[] }) {}
 
-	editorClient(vueComponent: any, socket: Socket | null) {
+	editorClient(vueComponent: any) {
+		const { socket } = this;
 		let plugin;
 		if (socket !== null) {
 			plugin = ViewPlugin.define(view => ({
@@ -86,6 +95,7 @@ export default class EditorService {
 								return serializedUpdate;
 							},
 						);
+						console.log(unsentUpdates);
 
 						socket.emit('clientOpUpdate', {
 							version: getSyncedVersion(view.state),
@@ -107,5 +117,88 @@ export default class EditorService {
 			}));
 		}
 		return plugin;
+	}
+
+	openSocketConnection() {
+		// this.socket = io('https://text-sockets.herokuapp.com/');
+		return io('http://localhost:8080/');
+	}
+
+	socketsCreateNewRoom(roomId: string) {
+		this.socket = this.openSocketConnection();
+		this.roomId = roomId;
+		this.doc = this.view.state.doc;
+
+		this.socket?.emit('create', {
+			roomId: this.roomId,
+			documentData: {
+				doc: this.view.state.doc,
+				updates: [],
+			},
+		});
+
+		this.socket?.on('created', documentData => {
+			console.log(documentData.doc, this.view.state);
+			this.vueComponent.editorService.setDocumentState(documentData);
+			if (this.vueComponent.view !== null) {
+				this.socket?.on('serverOpUpdate', changes => {
+					const deserializedChangeSet = changes.updates.map(
+						(u: { updateJSON: any; clientID: string }) => {
+							return {
+								changes: ChangeSet.fromJSON(u.updateJSON),
+								clientID: u.clientID,
+							};
+						},
+					);
+					this.view?.dispatch(
+						receiveUpdates(this.view.state, deserializedChangeSet),
+					);
+				});
+			}
+		});
+	}
+
+	socketsJoinRoom(roomId: string) {
+		this.roomId = roomId;
+		this.view.destroy();
+
+		this.socket?.emit('join', this.roomId);
+		this.socket?.on('joined', documentData => {
+			console.log(documentData.doc);
+			this.generateEditor(Text.of(documentData.doc));
+			if (this.vueComponent.view !== null) {
+				this.socket?.on('serverOpUpdate', changes => {
+					const deserializedChangeSet = changes.updates.map(
+						(u: { updateJSON: any; clientID: string }) => {
+							return {
+								changes: ChangeSet.fromJSON(u.updateJSON),
+								clientID: u.clientID,
+							};
+						},
+					);
+					this.view?.dispatch(
+						receiveUpdates(this.view.state, deserializedChangeSet),
+					);
+				});
+			}
+		});
+	}
+
+	static generateRoomId() {
+		let result = '';
+		const roomIdLength = 5;
+		const characters =
+			'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+		const charactersLength = characters.length;
+		for (let i = 0; i < roomIdLength; i++) {
+			result += characters.charAt(
+				Math.floor(Math.random() * charactersLength),
+			);
+		}
+		return result;
+	}
+
+	disconnect() {
+		this.socket?.close();
 	}
 }
