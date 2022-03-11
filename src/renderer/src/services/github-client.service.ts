dd5c676c0d0ee8ee6367978644bb2d4adeb5b7ec
Matteo Miceli <omatteomiceli@gmail.com>
2022-03-10 20:46:04 -0800

    git integration

--- src/renderer/src/services/github-client.service.ts
+++ src/renderer/src/services/github-client.service.ts
@@ -1,21 +1,22 @@
 import type { GitRepo } from '../../../typings/GitService';
 import type IGitClientService from '../Interfaces/IGitClientService';
 
-export default class GithubClientService implements IGitClientService {
+export default class GithubClientService /* implements IGitClientService */ {
 	username: string;
 
 	repositories: GitRepo[] = [];
 
 	constructor(username: string) {
 		this.username = username;
-		this.getRepos();
-	}
-
-	getRepos(): void {
 		window.ipcRenderer.send('github-connect', this.username);
 	}
 
-	getRepoContent(repoUrl: string) {
-		window.ipcRenderer.send('get-repo-content', repoUrl);
+	async getRepoList(): Promise<GitRepo[]> {
+		this.repositories = await window.ipcRenderer.invoke(
+			'get-repo-list',
+			this.username,
+		);
+
+		return this.repositories;
 	}
 }
