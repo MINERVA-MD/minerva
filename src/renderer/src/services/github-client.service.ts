77d02c67a890442006932438bfafa2a7259e8487
Matteo Miceli <omatteomiceli@gmail.com>
2022-03-10 23:01:39 -0800

    committed from Minerva!

--- src/renderer/src/services/github-client.service.ts
+++ src/renderer/src/services/github-client.service.ts
@@ -1,22 +1,28 @@
 import type { GitRepo } from '../../../typings/GitService';
-import type IGitClientService from '../Interfaces/IGitClientService';
+// import type IGitClientService from '../Interfaces/IGitClientService';
 
 export default class GithubClientService /* implements IGitClientService */ {
 	username: string;
 
-	repositories: GitRepo[] = [];
+	repo: string;
 
-	constructor(username: string) {
+	token: string;
+
+	userRepositories: GitRepo[] = [];
+
+	constructor(username: string, token: string) {
 		this.username = username;
-		window.ipcRenderer.send('github-connect', this.username);
+		this.repo = '';
+		this.token = token;
+		window.ipcRenderer.send('github-connect', this.username, this.token);
 	}
 
 	async getRepoList(): Promise<GitRepo[]> {
-		this.repositories = await window.ipcRenderer.invoke(
+		this.userRepositories = await window.ipcRenderer.invoke(
 			'get-repo-list',
 			this.username,
 		);
 
-		return this.repositories;
+		return this.userRepositories;
 	}
 }
