a33c9564d9d7525b0fd7f1740b17089129e1e280
Matteo Miceli <omatteomiceli@gmail.com>
2022-03-20 00:15:16 -0700

    refactored git service
    all IPC calls now live on git service class making it more declarative

--- src/renderer/src/Interfaces/IGitClientService.ts
+++ src/renderer/src/Interfaces/IGitClientService.ts
@@ -2,6 +2,10 @@ import type { GitRepo } from '@/typings/GitService';
 
 export default interface IGitClientService {
 	username: string;
+	repo: string;
+	token: string;
 	userRepositories: GitRepo[];
 	getRepoList: () => Promise<GitRepo[]>;
+	cloneSelectedRepo: () => void;
+	getReadMeContents: () => Promise<string>;
 }
