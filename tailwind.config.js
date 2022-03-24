b5f3135c378891e8209073da2d0a6d7c0fb807f2
Matteo Miceli <omatteomiceli@gmail.com>
2022-03-24 10:59:56 -0700

    added color minerva-purple

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
 	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'minerva-purple': '#5452C1',
+			},
+		},
 	},
 	plugins: [],
 };
