7eb9a4bb9940a09a836ed6fabc16bcfced4a61bf
Matteo Miceli <omatteomiceli@gmail.com>
2022-04-01 13:51:40 -0700

    footer component and porting gitservice new view

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -8,26 +8,31 @@
 	<RouterView v-slot="{ Component }">
 		<component :is="Component" ref="view" />
 	</RouterView>
+	<Footer />
 </template>
 
 <script lang="ts">
 import { RouterView, RouterLink } from 'vue-router';
 import { defineComponent } from 'vue-demi';
 import './css/index.css';
+import type GithubClientService from './services/github-client.service';
 import Editor from './views/Editor.vue';
 import Navbar from './components/NavBar.vue';
-import { VueElement } from 'vue-demi';
+import Footer from './components/Footer.vue';
 
 export default defineComponent({
 	components: {
 		Navbar,
 		Editor,
+		Footer,
 	},
 	data(): {
 		roomId: string | null;
+		gitService: GithubClientService | null;
 	} {
 		return {
 			roomId: '',
+			gitService: null,
 		};
 	},
 	methods: {
