b259d7b85be5e8b898b04442dd423ccbe40673c6
jfoxx77 <jsam07@hotmail.com>
2022-03-31 17:46:12 -0700

    Add router reference App.vue

---
+++ src/renderer/src/backup/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcomeVue from './components/TheWelcome.vue';
+</script>
+
+<template>
+	<header>
+		<!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" /> -->
+		<div class="logo-box">
+			<img class="logo" alt="Electron logo" style="height:140px;" src="./assets/electron.png" >
+			<span/>
+			<img class="logo" alt="Vite logo" style="height:140px;" src="./assets/vite.svg" >
+			<span/>
+			<img class="logo" alt="Vue logo" style="height:140px;" src="./assets/vue.png" >
+			<span/>
+			<img class="logo" alt="TypeScript logo" style="height:140px;" src="./assets/ts.svg" >
+		</div>
+		<div class="wrapper">
+			<HelloWorld msg="You did it!" />
+
+			<nav>
+				<RouterLink to="/">Home</RouterLink>
+				<RouterLink to="/about">About</RouterLink>
+			</nav>
+		</div>
+	</header>
+
+	<RouterView />
+</template>
+
+<style>
--- src/renderer/src/views/HomeView.vue
+++ src/renderer/src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts"></script>
 
 <template>
-	<main></main>
+	<main>This is the home view</main>
 </template>
