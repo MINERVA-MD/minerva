e8e30f4e5e5b6fa1b42d9c1505067af09880d327
matteomiceli <omatteomiceli@gmail.com>
2022-04-01 01:27:25 -0700

    functionality for menu/nav on app.vue - todo: register gitservice on app

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -15,19 +15,35 @@ import { RouterView, RouterLink } from 'vue-router';
 import './css/index.css';
 import Editor from './views/Editor.vue';
 import Navbar from './components/NavBar.vue';
+import { VueElement } from 'vue-demi';
 
 export default {
 	components: {
 		Navbar,
 		Editor,
 	},
-	data(): {} {
-		return {};
+	data(): {
+		roomId: string | null;
+	} {
+		return {
+			roomId: '',
+		};
 	},
 	methods: {
 		newBlankEditor() {
-			if (this.$refs.view.newBlankEditor) {
-				this.$refs.view.newBlankEditor();
+			if ((this.$refs.view as any).newBlankEditor) {
+				this.roomId = '';
+				(this.$refs.view as any).newBlankEditor();
+			}
+		},
+		createCollabSession() {
+			if ((this.$refs.view as any).createCollabSession)
+				this.roomId = (this.$refs.view as any).createCollabSession();
+		},
+		joinCollabSession(roomId: string) {
+			if ((this.$refs.view as any).joinCollabSession) {
+				this.roomId = roomId;
+				(this.$refs.view as any).joinCollabSession(roomId);
 			}
 		},
 	},
