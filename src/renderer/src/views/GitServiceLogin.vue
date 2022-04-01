8a3b99501fe0d853b6e7bc3f20565eb8354a8824
Matteo Miceli <omatteomiceli@gmail.com>
2022-04-01 13:51:40 -0700

    footer component and porting gitservice new view

--- src/renderer/src/views/GitServiceLogin.vue
+++ src/renderer/src/views/GitServiceLogin.vue
@@ -1,14 +1,40 @@
 <template>
+	<RouterLink class="text-minerva-purple" to="/">Back to editor</RouterLink>
 	<div>
-		<p>hi Im a login</p>
-		<RouterLink to="/">Back to editor</RouterLink>
+		<button
+			class="bg-minerva-purple px-2 py-1 text-white rounded hover:opacity-90 transition-all duration-100"
+			v-on:click="connectGit"
+		>
+			Connect Git
+		</button>
+		<select
+			name="repos"
+			id="repos"
+			v-model="repoSelect"
+			class="border rounded py-1"
+		>
+			<option default disabled value="">
+				{{ gitService ? 'Repositories' : 'No Git Service' }}
+			</option>
+			<option v-for="repo in repos" :value="repo.name" :key="repo.name">
+				{{ repo.name }}
+			</option>
+		</select>
 	</div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { RouterLink } from 'vue-router';
+import type GithubClientService from '../services/github-client.service';
 
 export default defineComponent({
 	setup() {},
+	data(): {
+		gitService: GithubClientService | null;
+	} {
+		return {
+			gitService: null,
+		};
+	},
 });
 </script>
