64fe36efa7b711e24593b2cb6f85689d01b09030
Matteo Miceli <matteo@pop-os.localdomain>
2022-04-11 20:49:17 -0700

    stashing changes
    todo - clear session data

--- src/common/utils/vault.util.ts
+++ src/common/utils/vault.util.ts
@@ -1,7 +1,11 @@
 import fs from 'fs';
+import path from 'path';
+import { app } from 'electron';
 
+// eslint-disable-next-line import/prefer-default-export
 export const getSecret = (key: string): string => {
-	const ENV_PATH = 'env.json';
+	// const ENV_PATH = 'env.json';
+	const ENV_PATH = path.join(app.getPath('documents'), '/Minerva/env.json');
 
 	if (fs.existsSync(ENV_PATH)) {
 		const secretsJSON = JSON.parse(
