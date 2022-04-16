afefe7d2fe209cfed63fdb9a7c9eba2b126dc294
matteomiceli <omatteomiceli@gmail.com>
2022-04-16 01:55:38 -0700

    conditional save disable based on whether file is loaded

--- src/main/services/fileHandle.service.ts
+++ src/main/services/fileHandle.service.ts
@@ -4,18 +4,25 @@ import fs from 'fs';
 export default class FileHandle {
 	static listen() {
 		ipcMain.handle('saveAsFile', (event, editorData) => {
-			this.saveAsFile(editorData);
+			return this.saveAsFile(editorData);
+		});
+
+		ipcMain.handle('saveFile', (event, filePath, editorData) => {
+			return this.saveFile(filePath, editorData);
 		});
 	}
 
 	static saveAsFile(data: string) {
 		const savePath = dialog.showSaveDialogSync({});
 		if (savePath) {
-			this.writeFileToPath(savePath, data);
+			fs.writeFileSync(savePath, data);
 		}
+		return savePath;
 	}
 
-	static writeFileToPath(path: string, data: string) {
-		fs.writeFileSync(path, data);
+	static saveFile(filePath: string, data: string) {
+		if (fs.existsSync(filePath)) {
+			fs.writeFileSync(filePath, data);
+		}
 	}
 }
