4fcc4c43d0904b810f5095cbec7e39190f0646ae
jfoxx77 <jsam07@hotmail.com>
2022-04-18 11:57:07 -0700

    Add splash screen

--- src/preload/utils.ts
+++ src/preload/utils.ts
@@ -1,15 +1,16 @@
-
 /** docoment ready */
-export function domReady(condition: DocumentReadyState[] = ['complete', 'interactive']) {
-  return new Promise(resolve => {
-    if (condition.includes(document.readyState)) {
-      resolve(true)
-    } else {
-      document.addEventListener('readystatechange', () => {
-        if (condition.includes(document.readyState)) {
-          resolve(true)
-        }
-      })
-    }
-  })
+export default function domReady(
+	condition: DocumentReadyState[] = ['complete', 'interactive'],
+) {
+	return new Promise(resolve => {
+		if (condition.includes(document.readyState)) {
+			resolve(true);
+		} else {
+			document.addEventListener('readystatechange', () => {
+				if (condition.includes(document.readyState)) {
+					resolve(true);
+				}
+			});
+		}
+	});
 }
