7723633fca06c21d9867e63a1165cdc1169ee2fe
jfoxx77 <jsam07@hotmail.com>
2022-05-30 00:33:48 -0700

    Add benchmark tests

--- src/main/services/parsers/marked.parser.ts
+++ src/main/services/parsers/marked.parser.ts
@@ -1,6 +1,22 @@
 import { marked } from 'marked';
-
+import hljs from 'highlight.js';
 import type IParser from '../../interfaces/IParser';
+
+marked.setOptions({
+	renderer: new marked.Renderer(),
+	highlight(code, lang) {
+		const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+		return hljs.highlight(code, { language }).value;
+	},
+	langPrefix: 'hljs language-', // highlight.js css expects a top-level 'hljs' class.
+	pedantic: false,
+	gfm: true,
+	breaks: false,
+	sanitize: false,
+	smartLists: true,
+	smartypants: false,
+	xhtml: false,
+});
 
 export default class MarkedParser implements IParser {
 	// eslint-disable-next-line class-methods-use-this
